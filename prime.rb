7d2965d61223d42de77443359d855a7d3bd5ac05
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:18:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num == 1
+    false
+  end
+
+  if num == 2
+    true
+  end
+
+  if num > 2
+    (2...num).each do |x|
+      if num % x == 0
+        false
+      else
+        true
+      end
+    end
+  end
+  true  
+end
