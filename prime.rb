2afe985f3d1b24c09160e4a375a49c32d8647914
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:40:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(num)
-  # if num == 0
-  #   return false
-  # end
-  #
+  if num == 0
+    return false
+  end
+  
   if num == 1 || num == -1
     return false
   end
