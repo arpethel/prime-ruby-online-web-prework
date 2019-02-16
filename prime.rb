d9ed8aed946fc903f5de9485b1173828658b5f1d
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:43:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,10 @@
 # Add  code here!
 def prime?(num)
   if num == 0
-    return false
+    false
+  elsif num == 1 || num == -1
+    false
   end
   
-  if num == 1 || num == -1
-    return false
-  end
-  #
-  # if num == 2 || num == -2
-  #   return true
-  # end
-
   (2..num/2).none? {|i| num % i == 0}
-
-  # if num > 2 || num < -2
-  #   (2...num).each do |x|
-  #     if num % x == 0
-  #       return false
-  #     end
-  #   end
-  #   true
-  # end
-  # return false
 end
